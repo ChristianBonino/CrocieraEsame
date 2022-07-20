f1f570df7a6b7f2e5f1f2a11c0ebaf91a3f0e54b
ChristianBonino <christianbonino@live.it>
2022-07-20 08:43:12 +0200

    Aggiungere i file di progetto.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,96 @@
+﻿using Crociera.DB;
+using Crociera.DB.Entities;
+using Crociera.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Crociera.Controllers
+{
+    public class HomeController : Controller
+    {
+        private SignInManager<User> signInManager;
+        private UserManager<User> userManager;
+        private UserDBContext dbContext;
+
+        public HomeController(SignInManager<User> signInManager,
+            UserManager<User> userManager,
+            UserDBContext dbContext)
+        {
+            this.signInManager = signInManager;
+            this.userManager = userManager;
+            this.dbContext = dbContext;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [Authorize]
+        public IActionResult HiddenPage()
+        {
+            return View();
+        }
+        public IActionResult Login()
+        {
+            LoginModel model = new LoginModel();
+            return View(model);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Login(LoginModel loginModel)
+        {
+            try
+            {
+               //User user = await userManager.FindByNameAsync(loginModel.UserName);
+               User user = await userManager.FindByEmailAsync(loginModel.Email);
+                if (user != null)
+                {
+                    var result = await signInManager.PasswordSignInAsync(user.UserName, loginModel.Password, false, lockoutOnFailure: true);
+                    if (result.Succeeded)
+                    {
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+            return Redirect("Index");
+        }
+
+        [Authorize]
+        public async Task<IActionResult> Logout()
+        {
+            try
+            {
+                if (signInManager.IsSignedIn(User))
+                {
+                    await signInManager.SignOutAsync();
+                }
+            }
+            catch (Exception ex)
+            {
+            }
+            return Redirect("Index");
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
